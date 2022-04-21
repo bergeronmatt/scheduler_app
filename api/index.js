8436f4b73f506a05c3b63904d5ccabfa15454f2d
Matt Bergeron <mbergeron@comresource.com>
2022-04-21 11:46:53 -0400

    new server architecture up and running

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 // set up environmental variable control
 require('dotenv').config()
 
+// import the server module
+const app = require('./server/server.js')
+
 // set up and require cross-origin policy ath
 // start of the api
 const cors = require('cors')
 app.use(cors())
-
-// import the server module
-const app = require('./server/server.js')
 
 // set up port object
 const port = process.env.PORT || 8080;
