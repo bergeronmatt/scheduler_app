a9e960fae3f08669104513b990dcb56685175efd
Matt Bergeron <mbergeron@comresource.com>
2022-04-30 21:40:27 -0400

    updated some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,21 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import './css/index.css';
 import Nav from './Components/Nav'
+import logo from "./img/mge-logo.png"
 
 function App() {
   return (
     <div className="App">
-      <h1>Scheduler App</h1>
+      <div>
+        <img 
+          src={logo} 
+          alt='logo.png' 
+          style={{
+            width: '15%',
+            margin: '1% 0%'
+          }} 
+        />
+      </div>
       <Nav />
     </div>
   );
