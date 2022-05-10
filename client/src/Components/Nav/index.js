72dd41f7d7e9e7d08d79b40c5960e1a713074321
Matt Bergeron <mbergeron@comresource.com>
2022-05-10 09:29:31 -0400

    backup commit

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -1,48 +1,59 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import Login from '../Login'
+import React from 'react'
+// import axios from 'axios'
+// import Login from '../Login'
 import Calendar from '../Calendar'
 
 import {
   BrowserRouter,
   Routes,
   Route,
-  Navigate,
-  Outlet
+  // Navigate,
+  // Outlet
 } from 'react-router-dom'
 
 export default function Nav() {
 
-  const [allowed, setAllowed] = useState(false)
+  /** This is the authentication code  */
 
-  const ProtectedRoute = ({isAllowed, redirectPath = '/',children }) => {
-    if(isAllowed === false){
-      return <Navigate to={redirectPath} replace />
-    }
-    return children ? children : <Outlet />
-  }
+  // const [allowed, setAllowed] = useState(false)
 
-  function verifyUser(token) {
-    axios.get('http://localhost:8080/api/validate', {headers: {Authorization: token}})
-      .then(res => {
-        if(res.status !== 200) {
-          console.error();
-          return;
-        }
-        setAllowed(true)
-      })
-  }
+  // const ProtectedRoute = ({isAllowed, redirectPath = '/',children }) => {
+  //   if(isAllowed === false){
+  //     return <Navigate to={redirectPath} replace />
+  //   }
+  //   return children ? children : <Outlet />
+  // }
 
-  useEffect(() => {
-    const token = localStorage.getItem('auth')
-    verifyUser(token)
-  }, [allowed])
+  // function verifyUser(token) {
+  //   axios.get('http://localhost:8080/api/validate', {headers: {Authorization: token}})
+  //     .then(res => {
+  //       if(res.status !== 200) {
+  //         console.error();
+  //         return;
+  //       }
+  //       setAllowed(true)
+  //     })
+  // }
+
+  // useEffect(() => {
+  //   const token = localStorage.getItem('auth')
+  //   verifyUser(token)
+  // }, [allowed])
 
   return (
     <div className='wrapper'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Login allowed={allowed} />}/>
+
+        <Route path='/' element={<Calendar/>}/>
+
+          {/* 
+            This is the original route build with a login page and 
+            protected path to the calendar. Keep this and reuse if
+            there is a need for authentication
+          */}
+
+          {/* <Route path='/' element={<Login allowed={allowed} />}/>
           <Route path='/calendar' element={
             <ProtectedRoute
               redirectPath="/"
@@ -52,7 +63,7 @@ export default function Nav() {
                 <Calendar />
               </div>
             </ProtectedRoute>
-          } />
+          } /> */}
         </Routes>
       </BrowserRouter>
     </div>
